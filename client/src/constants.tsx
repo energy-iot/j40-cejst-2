6c3e4536bbf1bf9ce676ce93783c560169184187
gitemdb <gitemdb@example.com>
2025-10-08 18:08:47 -0700

    fix styling on constants - thanks linter :)

--- client/src/constants.tsx
+++ client/src/constants.tsx
@@ -1,3 +1,3 @@
 export const GITHUB_LINK =
-  "https://github.com/Public-Environmental-Data-Partners/j40-cejst-2";
+  'https://github.com/Public-Environmental-Data-Partners/j40-cejst-2';
 export const GITHUB_LINK_ES = `${GITHUB_LINK}/blob/main/README-es.md`;
